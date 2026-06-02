39c907665be8a922e591fa7a67e823c2ff857092
prakharsharma13 <prakharsharma752@gmail.com>
2026-06-03 00:25:43 +0530

    closures in js

---
+++ 13_closures/closures.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func counter() func() int {
+	var count int = 0
+
+	return func() int {
+		count = count + 1
+		return count
+	}
+}
+
+func main() {
+	increment := counter()
+
+	fmt.Println(increment())
+}
